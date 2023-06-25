9b01ae58a7b0556e1fa909ae2e11fcbdf55caf29
Facundo <facuscardillo@gmail.com>
2023-06-25 19:04:55 -0300

    Create repositoryList component

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
 import React from 'react'
 
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import Main from './src/components/Main'
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Hi! My first React Native APP</Text>
-      <StatusBar style='auto' />
-    </View>
+    <Main />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import respositories from '../data/respositories'
+
+const RepositoryList = () => {
+  return (
+    <View>
+      {respositories.map(repo => (
+        <View key={repo.id}>
+          <Text>{repo.fullName}</Text>
+        </View>
+      ))}
+    </View>
+  )
+}
+
+export default RepositoryList
