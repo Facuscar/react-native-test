4430f847a819013f02b6cf527ccd7d24d043c4fc
Facundo <facuscardillo@gmail.com>
2023-07-02 20:10:46 -0300

    Styles to the RepositoryItem

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,34 +1,66 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Image } from 'react-native'
 
 import StyledText from './StyledText'
 import RepositoryStats from './RepositoryStats'
 import { theme } from '../theme'
 
-const RepositoryItem = ({ repo }) => {
-  const styles = StyleSheet.create({
-    container: {
-      padding: 20,
-      paddingBottom: 5,
-      paddingTop: 5
-    },
-    language: {
-      padding: 4,
-      color: theme.colors.white,
-      backgroundColor: theme.colors.primary,
-      alignSelf: 'flex-start',
-      borderRadius: 4,
-      overflow: 'hidden'
-    }
-  })
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 5,
+    paddingTop: 5
+  },
+  language: {
+    padding: 4,
+    color: theme.colors.white,
+    backgroundColor: theme.colors.primary,
+    alignSelf: 'flex-start',
+    borderRadius: 4,
+    overflow: 'hidden',
+    marginVertical: 4
+  },
+  image: {
+    width: 48,
+    height: 48,
+    margin: 'auto',
+    borderRadius: 4
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    paddingBottom: 2
+  },
+  subheadingContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  imageContainer: {
+    paddingRight: 10
+  }
+})
 
-  return (
-    <View key={repo.id} style={styles.container}>
-      <StyledText fontSize='subheading' fontWeight='bold'>{repo.fullName}</StyledText>
-      <StyledText>{repo.description}</StyledText>
-      <StyledText style={styles.language}>{repo.language}</StyledText>
-      <RepositoryStats repo={repo} />
+const RepositoryItemHeader = ({ ownerAvatarUrl, fullName, description, language }) => (
+  <View style={styles.headerContainer}>
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
     </View>
-  )
-}
+    <View style={styles.subheadingContainer}>
+      <StyledText fontSize='subheading' fontWeight='bold'>{fullName}</StyledText>
+      <StyledText>{description}</StyledText>
+      <StyledText style={styles.language}>{language}</StyledText>
+    </View>
+  </View>
+)
+
+const RepositoryItem = ({ repo }) => (
+  <View key={repo.id} style={styles.container}>
+    <RepositoryItemHeader
+      ownerAvatarUrl={repo.ownerAvatarUrl}
+      fullName={repo.fullName}
+      description={repo.description}
+      language={repo.language}
+    />
+    <RepositoryStats repo={repo} />
+  </View>
+)
 export default RepositoryItem
