a683b3be73dd41c673de7700079b4ae29d43ee5f
Facundo <facuscardillo@gmail.com>
2023-07-15 22:38:27 -0300

    Create basic form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Formik } from 'formik'
+import { Button, StyleSheet, View } from 'react-native'
+
+import StyledTextInput from '../components/StyledTextInput'
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 12
+  }
+})
 
 const LogIn = () => {
+  const initialValues = {
+    email: '',
+    password: ''
+  }
   return (
-    <Text>
-      Working on log in form
-    </Text>
+    <Formik initialValues={initialValues} onSubmit={(values) => console.log(values)}>
+      {({ handleChange, handleSubmit, values }) => {
+        return (
+          <View style={styles.form}>
+            <StyledTextInput
+              placeholder='E-mail'
+              value={values.email}
+              onChangeText={handleChange('email')}
+            />
+            <StyledTextInput
+              value={values.password}
+              onChangeText={handleChange('password')}
+            />
+            <Button title='Log in' onPress={handleSubmit} />
+          </View>
+        )
+      }}
+    </Formik>
   )
 }
 
